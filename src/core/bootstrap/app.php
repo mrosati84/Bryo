0b02af6be601e9271c42d79fcc7f20a048669e4b
Matteo Rosati <mrosati@h-art.com>
2016-03-10 23:34:12 +0100

    bind interface implementation

--- src/core/bootstrap/app.php
+++ src/core/bootstrap/app.php
@@ -50,6 +50,12 @@ $app->singleton(
     Bryo\Console\Kernel::class
 );
 
+// Register binding for database implementation.
+// We will use MongoDB as a default implementation but
+// we want to be flexible and provide other implementations
+// in the future.
+$app->bind('Bryo\Database\Contracts\Database', 'Bryo\Database\MongoDatabase');
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
