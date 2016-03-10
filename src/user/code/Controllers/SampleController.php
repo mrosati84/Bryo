c5085279bdde7499fe855023f9bcdc1dd35379a1
Matteo Rosati <mrosati@h-art.com>
2016-03-10 23:34:33 +0100

    inject database dependency

--- src/user/code/Controllers/SampleController.php
+++ src/user/code/Controllers/SampleController.php
@@ -3,12 +3,17 @@
 namespace App\Controllers;
 
 use Bryo\Http\Controllers\Controller;
+use Bryo\Database\Contracts\Database;
 
 class SampleController extends Controller
 {
-    public function index() {
+    public function index(Database $db) {
         // We can access request easily.
         $request = $this->getRequest();
+
+        // Connect to the database using
+        // injected database implementation.
+        $db->connect();
 
         return 'SampleController@index';
     }
